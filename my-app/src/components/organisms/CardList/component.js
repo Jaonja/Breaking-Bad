ba5ad6b2fe4cbe4c95dfefd6d7cfc599e1cebe38
yaros <golishon0@gmail.com>
2021-12-27 16:13:52 +0300

    Tab

--- my-app/src/components/organisms/CardList/component.js
+++ my-app/src/components/organisms/CardList/component.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import Card from '../../molecules/Card/index'
-import styles from './styles.module.scss'
+import React from "react";
+import Card from "../../molecules/Card/index";
+import styles from "./styles.module.scss";
 
-const CardList  = ({cards}) => {
-
+const CardList = ({ cards }) => {
   return (
     <div>
       <div className={styles.cardList}>
         {cards.map((card) => (
           <Card
-					key={card.uuid}
-					name={card.name}
-					birthday={card.birthday}
-					img={card.img}
-					status={card.status}
+            key={card.uuid}
+            name={card.name}
+            birthday={card.birthday}
+            img={card.img}
+            status={card.status}
           />
         ))}
       </div>
@@ -21,4 +20,4 @@ const CardList  = ({cards}) => {
   );
 };
 
-export default CardList ;
+export default CardList;
--- my-app/src/components/organisms/CardList/container.js
+++ my-app/src/components/organisms/CardList/container.js
@@ -3,8 +3,8 @@ import { useSelector } from "react-redux";
 import CardList from "./component";
 
 const CardListContainer = () => {
- const cards = useSelector((state) => state.cardListReducer.cards);
- return <CardList cards={cards} />;
+  const cards = useSelector((state) => state.cardListReducer.cards);
+  return <CardList cards={cards} />;
 };
 
 export const container = CardListContainer;
