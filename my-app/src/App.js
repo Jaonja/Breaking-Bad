0dc749da43719cd3424849285985fc3e39647f26
yaros <golishon0@gmail.com>
2022-01-19 18:30:42 +0300

    Revert "Merge pull request #1 from Jaonja/Feature/BB-1"
    This reverts commit 1ffc2f7250ae162ca1cd7da349b8eb8b935cc1e9, reversing
    changes made to 9640efcbb7fa574068813195982a52126dadbba8.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,8 @@
-import React from "react";
-import CardList from "./components/organisms/CardList";
+import React from 'react'
 
 function App() {
   return (
-    <div className="mein">
-      <CardList />
+    <div className="App">
     </div>
   );
 }
