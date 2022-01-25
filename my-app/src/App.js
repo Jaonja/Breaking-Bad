ba9dc8fa9e97a29aaa6c0dc400e2bb839209b6c7
yaros <golishon0@gmail.com>
2022-01-25 17:22:10 +0300

    Route Routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
-import CardList from "./components/organisms/CardList";
+import { Route, Routes, Link } from "react-router-dom";
+import "./assets/styles/styles.scss";
+import MeinPage from "./components/pages/meinPage";
+import CharactersListPage from "./components/pages/charactersListPage/";
+import CharacterPage from "./components/pages/characterPage/";
+import styles from "./styles.module.scss";
 
 function App() {
   return (
-    <div className="mein">
-      <CardList />
-    </div>
+    <>
+      <header className={styles.header}>
+        <Link to="/">HomePage</Link>
+        <Link to="/list">list</Link>
+        <Link to="/character">character</Link>
+      </header>
+
+      <Routes>
+        <Route path="/" element={<MeinPage />} />
+        <Route path="/list" element={<CharactersListPage />} />
+        <Route path="/character" element={<CharacterPage />} />
+      </Routes>
+    </>
   );
 }
 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,13 +1,15 @@
-
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import store from "./store/index";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />,
+    <BrowserRouter>
+      <App />,
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
